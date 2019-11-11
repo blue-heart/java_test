3d26e45a3a3b8b475533012ef9f28a67aa50289b
13088952942 <2278455677@qq.com>
2019-11-11 16:28:17 +0800

    插入排序

---
+++ 插入排序/TestDemo1.java
@@ -0,0 +1,48 @@
+package py.dm;
+
+/*
+时间复杂度n*n-1
+o（n^2）
+空间复杂度o（1）
+如果给定的数据是有序的数据，那么时间复杂度
+就是o（n）
+直接插入排序：越有序 越快
+稳定性：因为break的缘故
+是稳定性的排序
+ */
+public class TestDemo1 {
+    public static void insertSort(int[] array) {
+        int tmp = 0;
+        //这样就把j定义到了外面
+        //为了公用
+        int j;
+        for (int i = 1; i <array.length ; i++) {
+            tmp = array[i];
+            for ( j = i-1; j >=0 ; j--) {
+                if (array[j]>tmp){
+                    //因为多个数一起挪
+                    //不能用array【i】
+                    //j一直在走
+                    array[j+1] = array[j];
+                    //有序就不走了
+                }else {
+                    break;
+                }
+            }
+            //最后要把值再给j+1；
+            //这句语句虽然赋值两次
+            //但是为了 刚好拍完序那种情况 必须有
+           // array[j+1] = tmp;
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] array1 = {2,5,68,9,3,1,5,69,2};
+        insertSort(array1);
+        for (int i = 0; i <array1.length; i++) {
+            System.out.println(i);
+
+        }
+
+    }
+}
