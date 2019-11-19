4627221b444d4b97b4060c7439219c37a75104ba
13088952942 <2278455677@qq.com>
2019-11-19 18:00:13 +0800

    mysql查询复合查询

--- mysql查询复合查询/java9.sql
+++ mysql查询复合查询/java9.sql
@@ -1,4 +1,4 @@
-有一个商店的数据，记录客户及购物情况，有以下三个表组成：
+/*有一个商店的数据，记录客户及购物情况，有以下三个表组成：
 商品goods(商品编号goods_id，商品名goods_name, 单价unitprice, 商品类别category, 供应商provider)
 客户customer(客户号customer_id,姓名name,住址address,邮箱email,性别sex，身份证card_id)
 购买purchase(购买订单号order_id,客户号customer_ id,商品号goods_ id,购买数量nums)
@@ -157,7 +157,7 @@ select ename,deptno,job,sal
 from emp
 where deptno =(select deptno from emp where ename='SMITH')
 and job=(select job from emp where ename='SMITH');
-
+*/
 
 
 
